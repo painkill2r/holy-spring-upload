5ad343f00a4d9d21a1965198989be3c51afda845
wdEffort <yoman3011@gmail.com>
2021-08-01 16:11:59 +0900

    상품 등록 예제 - 이미지 출력 및 파일 다운로드 처리

--- src/main/java/hello/upload/controller/ItemController.java
+++ src/main/java/hello/upload/controller/ItemController.java
@@ -6,16 +6,19 @@ import hello.upload.domain.UploadFile;
 import hello.upload.file.FileStore;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriUtils;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Slf4j
@@ -65,5 +68,45 @@ public class ItemController {
         model.addAttribute("item", item);
 
         return "item-view";
+    }
+
+    /**
+     * 이미지 파일 출력
+     * 보안적인 문제는 무시하였으니, 실제 사용시에는 다른 방법을 사용하는 것을 권장함.
+     *
+     * @param filename
+     * @return
+     * @throws MalformedURLException
+     */
+    @ResponseBody
+    @GetMapping("/images/{filename}")
+    public Resource downloadImage(@PathVariable String filename) throws MalformedURLException {
+        //"file:/Users/yoman/.../a8ea61a6-d7a8-42fd-a388-203bc59bca76.jpeg"
+        return new UrlResource("file:" + fileStore.getFullPath(filename));
+    }
+
+    /**
+     * 첨부 파일 다운로드
+     *
+     * @param itemId 추후 유저 권한별로 상품에 등록된 첨부파일을 다운로드 할 수 있는지, 없는지에 대한 로직을 구현하기 위해 사용.
+     * @return
+     */
+    @GetMapping("/attach/{itemId}")
+    public ResponseEntity<Resource> downloadAttach(@PathVariable Long itemId) throws MalformedURLException {
+        Item item = itemRepository.findById(itemId);
+        String storeFileName = item.getAttachFile().getStoreFileName();
+        String uploadFileName = item.getAttachFile().getUploadFileName();
+        UrlResource urlResource = new UrlResource("file:" + fileStore.getFullPath(storeFileName));
+
+        log.info("uploadFileName={}", uploadFileName);
+
+        //한글 깨짐 처리
+        String encodeUploadFileName = UriUtils.encode(uploadFileName, StandardCharsets.UTF_8);
+        //파일을 다운로드 받을 수 있게 Header 설정(안그러면 다운로드가 아니라 그냥 열림.)
+        String contentDisposition = "attachment; filename=\"" + encodeUploadFileName + "\"";
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
+                .body(urlResource);
     }
 }
